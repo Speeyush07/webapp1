4452d4a7efd66a4a06ddaa35cdba359f1eb23059
Speeyush07 <Speeyush.1.S@niit-tech.com>
2020-04-03 12:52:24 +0530

    third commit

--- WebApplication1/Controllers/studentController.cs
+++ WebApplication1/Controllers/studentController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class StudentController : Controller
+    public class studentController : Controller
     {
         public IActionResult Index()
         {
-            List<Student> stlist = new List<Student>()
+            List<student> stlist = new List<student>()
             {
-                new Student{SId=101, SName="Peeyush", SMarks=95},
-                new Student{SId=102, SName="Arsh", SMarks=96},
-                new Student{SId=103, SName="Nikhil", SMarks=97},
+                new student{SId=101, SName="Peeyush", SMarks=95},
+                new student{SId=102, SName="Arsh", SMarks=96},
+                new student{SId=103, SName="Nikhil", SMarks=97},
             };
             return View(stlist);
         }
---
+++ WebApplication1/Models/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class student
+    {
+       public int SId { get; set; }
+        public string SName { get; set; }
+        public float SMarks { get; set; }
+    }
+}
