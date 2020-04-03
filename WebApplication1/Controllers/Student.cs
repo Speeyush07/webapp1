7c49aa01cc0797db2c60aa9abd12c6c8ffe55ee4
Speeyush07 <Speeyush.1.S@niit-tech.com>
2020-04-03 11:21:15 +0530

    secondcommit

---
+++ WebApplication1/Controllers/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Controllers
+{
+    internal class Student
+    {
+        public int SId { get; internal set; }
+        public string SName { get; internal set; }
+        public int SMarks { get; internal set; }
+    }
+}
--- WebApplication1/Controllers/studentController.cs
+++ WebApplication1/Controllers/studentController.cs
@@ -6,11 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class studentController : Controller
+    public class StudentController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            List<Student> stlist = new List<Student>()
+            {
+                new Student{SId=101, SName="Peeyush", SMarks=95},
+                new Student{SId=102, SName="Arsh", SMarks=96},
+                new Student{SId=103, SName="Nikhil", SMarks=97},
+            };
+            return View(stlist);
         }
     }
 }
